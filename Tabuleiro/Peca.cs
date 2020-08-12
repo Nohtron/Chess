f796a58c190b3489fc2b826282510570abe87701
Northon T. Cavazzoni <northoncavazzoni@hotmail.com>
2020-08-12 18:02:42 -0300

    Criada a classe Tabuleiro

---
+++ Tabuleiro/Peca.cs
@@ -0,0 +1,18 @@
+﻿namespace Tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QuantidadeDeMovimentos { get; protected set; }
+        public Tabuleiro Tabuleiro { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            Tabuleiro = tabuleiro;
+            QuantidadeDeMovimentos = 0;
+        }
+    }
+}
